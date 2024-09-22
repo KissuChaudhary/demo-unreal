9cdac9effe152b8aef0aa904915b3891824cd941
picshotai <support@picshotai.com>
2024-09-22 19:20:55 +0530

    Update StripeTable.tsx

--- components/stripe/StripeTable.tsx
+++ components/stripe/StripeTable.tsx
@@ -165,13 +165,13 @@ const StripePricingTable = ({ user }: Props) => {
         id="razorpay-checkout-js"
         src="https://checkout.razorpay.com/v1/checkout.js"
       />
-      <div className="grid grid-cols-1 px-4 gap-4 sm:grid-cols-3 md:gap-8">
+      <div className="grid grid-cols-1 px-4 py-4 gap-4 sm:grid-cols-3 md:gap-8">
         {Object.keys(plans).map((planKey) => {
           const plan = plans[planKey as PlanType];
           return (
             <div
               key={planKey}
-              className="grid grid-cols-1 px-4 py-4 gap-4 sm:grid-cols-3 md:gap-8"
+              className="rounded-xl bg-white border border-gray-200 p-6 shadow-sm sm:px-8 lg:p-12"
             >
               <div className="text-center">
                 <h2 className="text-lg font-medium text-gray-900">
