696071eff60e415e4fd129395bd13a4e8b91181f
picshotai <support@picshotai.com>
2024-09-22 23:28:38 +0530

    Update page.tsx

--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from 'react';
+
 import ActionButtons from "./action-buttons";
 import Logo from "./logo";
 import { NavigationMenuBar } from "./navigation-bar";
