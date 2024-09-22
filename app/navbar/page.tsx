113d21bada1555659d5167a0332165acf8c2496b
picshotai <support@picshotai.com>
2024-09-22 18:58:13 +0530

    Update page.tsx

--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -117,10 +117,7 @@ export default async function Navbar() {
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuGroup>
-                  <DropdownMenuItem>
-                    <User className="mr-2 h-4 w-4" />
-                    <span>Profile</span>
-                  </DropdownMenuItem>
+                 
                   <Link href="/overview">
                     <DropdownMenuItem>
                       <LayoutDashboard className="mr-2 h-4 w-4" />
