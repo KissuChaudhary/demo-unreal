fe08fb054d1208d3ea6c4a730bbdb909efc183ba
picshotai <support@picshotai.com>
2024-09-22 14:06:32 +0530

    Update page.tsx

--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -52,7 +52,14 @@ export default async function Navbar() {
         {/* Navigation buttons for logged-in users */}
         {user && (
           <div className="hidden lg:flex flex-row gap-2">
-            <Link href="/">
+             <Link href="/">
+              <Button
+                variant={"ghost"}
+                className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-md transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 font-medium"
+              >
+                <Home className="mr-2 h-4 w-4" />
+                Home
+              </Button>
             </Link>
             <Link href="/overview">
               <Button
