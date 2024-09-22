91df378f0e87f9cd2f47f6e1cbe59eae8ae41f20
picshotai <support@picshotai.com>
2024-09-23 00:06:45 +0530

    Update LaunchPriceBanner.tsx

--- app/navbar/LaunchPriceBanner.tsx
+++ app/navbar/LaunchPriceBanner.tsx
@@ -14,7 +14,7 @@ const LaunchPriceBanner = () => {
     >
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-center">
-          <span className="font-bold text-indigo-600 mr-2">30% off Launch Price!</span>
+          <span className="font-bold text-indigo-600 mr-2">30% off Basic Plan - Launch Price!</span>
         </div>
         <div className="mt-1 relative">
           <div className="inline-block relative" style={{ width: '40px' }}>
@@ -29,14 +29,14 @@ const LaunchPriceBanner = () => {
               style={{
                 borderRadius: '5px',
                 zIndex: 1000,
-                width: 'calc(100% + 5px)',
+                width: 'calc(100% + 1.5em)',
               }}
             >
               <span className="font-bold text-indigo-500 text-lg">$15.49</span>
             </div>
             <span className="absolute left-[-15px] top-0 line-through text-indigo-400 text-lg font-semibold">$21.99</span>
           </div>
-          <span className="text-indigo-400 text-xs ml-1 font-semibold">/ 04 Credits</span>
+          <span className="text-indigo-400 text-xs ml-1 font-semibold">  / 04 Credits</span>
         </div>  
       </div>
     </div>
