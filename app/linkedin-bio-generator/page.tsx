1119147ce45063109f85eea12296094cda52f7de
picshotai <support@picshotai.com>
2024-09-30 19:07:06 +0530

    Create page.tsx

---
+++ app/linkedin-bio-generator/page.tsx
@@ -0,0 +1,9 @@
+import LinkedInBioForm from '@/components/LinkedInBioForm';
+
+export default function LinkedInBioPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-indigo-100 to-purple-200 py-12 px-4 sm:px-6 lg:px-8">
+      <LinkedInBioForm />
+    </div>
+  );
+}
