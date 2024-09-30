7eee2b3f7e793fe6e5ef5cd5d90795ade9ecc6fe
picshotai <support@picshotai.com>
2024-09-30 19:54:45 +0530

    Update page.tsx

--- app/linkedin-bio-generator/page.tsx
+++ app/linkedin-bio-generator/page.tsx
@@ -1,9 +1,35 @@
 import Head from 'next/head';
 import LinkedInBioForm from '@/components/LinkedInBioForm';
-import Script from "next/script";
 
+export const getServerSideProps = async () => {
+  return {
+    props: {
+      schemaWebPage: {
+        "@context": "https://schema.org",
+        "@type": "WebPage",
+        "name": "LinkedIn Bio Generator",
+        "description": "Generate professional LinkedIn bios easily with our LinkedIn bio generator tool.",
+        "url": "https://unrealshot.com/linkedin-bio-generator"
+      },
+      schemaWebApplication: {
+        "@context": "https://schema.org",
+        "@type": "WebApplication",
+        "name": "LinkedIn Bio Generator",
+        "description": "An easy-to-use tool for generating LinkedIn bios.",
+        "applicationCategory": "BusinessApplication",
+        "operatingSystem": "All",
+        "url": "https://unrealshot.com/linkedin-bio-generator",
+        "offers": {
+          "@type": "Offer",
+          "price": "0.00",
+          "priceCurrency": "USD"
+        }
+      }
+    }
+  }
+}
 
-export default function LinkedInBioPage() {
+export default function LinkedInBioPage({ schemaWebPage, schemaWebApplication }: any) {
   return (
     <>
       <Head>
@@ -14,33 +40,10 @@ export default function LinkedInBioPage() {
         />
 
         {/* WebPage schema */}
-        <script type="application/ld+json">
-          {JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "WebPage",
-            "name": "LinkedIn Bio Generator",
-            "description": "Generate professional LinkedIn bios easily with our LinkedIn bio generator tool.",
-            "url": "https://yourwebsite.com/linkedin-bio-generator"
-          })}
-        </script>
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaWebPage) }} />
 
         {/* WebApplication schema */}
-        <script type="application/ld+json">
-          {JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "WebApplication",
-            "name": "LinkedIn Bio Generator",
-            "description": "An easy-to-use tool for generating LinkedIn bios.",
-            "applicationCategory": "BusinessApplication",
-            "operatingSystem": "All",
-            "url": "https://yourwebsite.com/linkedin-bio-generator",
-            "offers": {
-              "@type": "Offer",
-              "price": "0.00",
-              "priceCurrency": "USD"
-            }
-          })}
-        </script>
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaWebApplication) }} />
       </Head>
       <div className="min-h-screen px-4 sm:px-6 lg:px-8" style={{ paddingBottom: '3rem' }}>
         <LinkedInBioForm />
