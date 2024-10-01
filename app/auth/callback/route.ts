1ad9138054486802f98f5cda3c6629bdf2e20872
picshotai <support@picshotai.com>
2024-10-01 07:19:33 +0530

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,7 +2,6 @@ import { Database } from "@/types/supabase";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-
 import type { NextRequest } from "next/server";
 
 export const dynamic = "force-dynamic";
@@ -16,6 +15,7 @@ export async function GET(request: NextRequest) {
     const supabase = createRouteHandlerClient<Database>({
       cookies: () => cookieStore,
     });
+    
     await supabase.auth.exchangeCodeForSession(code);
   }
 
