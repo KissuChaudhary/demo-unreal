fe167d69151aa49c83b0ee05b5826e92666658a2
picshotai <support@picshotai.com>
2024-10-02 17:13:14 +0530

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-indigo-900 text-primary-foreground shadow hover:bg-indigo/800",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
