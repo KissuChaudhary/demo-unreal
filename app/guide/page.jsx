511d7abfe1725eede517b11d6e8f6d4c2319c081
picshotai <support@picshotai.com>
2024-10-03 23:22:01 +0530

    Update page.jsx

--- app/guide/page.jsx
+++ app/guide/page.jsx
@@ -1,5 +1,8 @@
 // app/guide/page.jsx
+"use client";
 import React from 'react';
+import Link from "next/link";
+import Image from 'next/image';
 
 const FAQItem = ({ question, answer }) => (
   <div className="mb-4">
