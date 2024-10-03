75dadf3b4883b185ecb44ffb48280da5cd8329b4
picshotai <support@picshotai.com>
2024-10-03 18:19:28 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
   experimental: {
     esmExternals: "loose",
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*/",
-        destination: "/:path*", // Redirect to the non-slash version
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
