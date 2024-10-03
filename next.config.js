1b73dc784d18897f0e9b1a9ed806343a0bdbf49b
picshotai <support@picshotai.com>
2024-10-03 18:08:27 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
   experimental: {
     esmExternals: "loose",
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "query",
+            key: "path",
+            value: "(.+[^/])$",  // This ensures non-slash URLs are redirected
+          },
+        ],
+        destination: "/:path*/", // Redirect to the version with the slash
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
