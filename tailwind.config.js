e83cb67844c8159ac904d8d62fbf309318db677b
picshotai <support@picshotai.com>
2024-10-06 09:54:48 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,9 +72,8 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    require('@tailwindcss/typography'),
-  ],
+ plugins: [require("tailwindcss-animate")],
   
-};
+};"
+
+
